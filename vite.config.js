ee860ab3c52742b2607ea7fbf644a14bfaf69666
Antonio Del Aguila <56671752+JorgeAntonio@users.noreply.github.com>
2022-12-20 19:15:45 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://JorgeAntonio.github.io/react-team-member-allocation/',
+  base: 'https://JorgeAntonio.github.io/react-team-member/',
   plugins: [react()]
 })
